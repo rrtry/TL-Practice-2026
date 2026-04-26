ad4a57c60b835d2d7f0dde1699c490aa2eb429e0
rrtry <fedormocalov36@gmail.com>
2026-04-26 03:05:43 +0300

    Added OrderManager and Casino solutions

---
+++ Casino/Casino/Program.cs
@@ -0,0 +1,134 @@
+﻿Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+PrintHeader();
+
+decimal balance = 0;
+bool isGameFinished = false;
+
+while ( !isGameFinished )
+{
+    PrintMenu();
+
+    string option = Console.ReadLine()!;
+    OptionHandleResult result = HandleOptions( option );
+
+    Console.WriteLine( result );
+    Console.WriteLine();
+}
+
+void PrintHeader()
+{
+    const string header = "Casino\n";
+    Console.WriteLine( header );
+}
+
+void PrintMenu()
+{
+    List<string> menuOptions = [
+        "1. Make Deposit",
+        "2. Show Balance",
+        "3. Play",
+        "4. Exit"
+    ];
+
+    foreach ( string option in menuOptions )
+    {
+        Console.WriteLine( option );
+    }
+}
+
+OptionHandleResult HandleOptions( string option )
+{
+    switch ( option )
+    {
+        case "1":
+            return MakeDeposit();
+        case "2":
+            return ShowBalance();
+        case "3":
+            return Play();
+        case "4":
+            return Exit();
+        default:
+            return OptionHandleResult.InvalidOption;
+    }
+}
+
+OptionHandleResult MakeDeposit()
+{
+    Console.Write( "Enter amount: " );
+    string depositString = Console.ReadLine()!;
+
+    if ( !decimal.TryParse( depositString, out decimal deposit ) || deposit <= 0 )
+    {
+        return OptionHandleResult.InvalidDepositValue;
+    }
+
+    balance += deposit;
+    return OptionHandleResult.Success;
+}
+
+OptionHandleResult ShowBalance()
+{
+    Console.WriteLine( $"Current balance is: {balance}" );
+    return OptionHandleResult.Success;
+}
+
+OptionHandleResult Play()
+{
+    Console.Write( "Your bet is: " );
+    string betStr = Console.ReadLine()!;
+
+    if ( !decimal.TryParse( betStr, out decimal bet ) || bet <= 0 )
+    {
+        return OptionHandleResult.InvalidBet;
+    }
+
+    if ( bet > balance )
+    {
+        return OptionHandleResult.InvalidBet;
+    }
+
+    int seed = Random.Shared.Next( 1, 21 );
+    if ( seed >= 18 && seed <= 20 )
+    {
+        decimal winAmount = CalculateWinAmount( bet, seed );
+        balance += winAmount;
+        Console.WriteLine( "You won!" );
+    }
+    else
+    {
+        balance -= bet;
+        Console.WriteLine( "You lost" );
+    }
+
+    return OptionHandleResult.Success;
+}
+
+decimal CalculateWinAmount( decimal bet, int seed )
+{
+    const int multiplicator = 25;
+    decimal winPrecent = multiplicator * ( seed % 17 );
+
+    if ( winPrecent <= 0 )
+    {
+        return 0;
+    }
+
+    return bet * ( winPrecent / 100 );
+}
+
+OptionHandleResult Exit()
+{
+    isGameFinished = true;
+    return OptionHandleResult.Success;
+}
+
+enum OptionHandleResult
+{
+    Success = 0,
+    InvalidOption = 1,
+    InvalidDepositValue = 2,
+    InvalidBet = 3
+}
---
+++ OrderManager/OrderManager/Program.cs
@@ -0,0 +1,159 @@
+﻿const string PROMPT_PRODUCT_TITLE = "Введите название товара: ";
+const string PROMPT_PRODUCT_QUANTITY = "Введите количество товара: ";
+const string PROMPT_USER_NAME = "Введите ваше имя: ";
+const string PROMPT_USER_ADDRESS = "Введите адрес доставки: ";
+const string PROMPT_CONTINUE_SHOPPING = "Продолжить покупки? (y/n): ";
+
+const string MESSAGE_ERROR_EMPTY_STRING = "Ошибка: значение не может быть пустым. Попробуйте снова.";
+const string MESSAGE_ERROR_INVALID_QUANTITY = "Ошибка: неверное число товаров. Попробуйте снова.";
+const string MESSAGE_ERROR_EMPTY_STDIN = "Ошибка: поток ввода пустой";
+const string MESSAGE_ERROR_PROMPT_ANSWER = "Ошибка: Ответьте да/нет";
+
+const string MESSAGE_ORDER_CANCELLED = "Заказ отменён.";
+const string DATE_FORMAT = "dd.MM.yyyy";
+
+string product;
+int quantity;
+
+string userName;
+string address;
+
+SetConsoleEncoding();
+Run();
+
+void Run()
+{
+    bool run = true;
+    while ( run )
+    {
+        ReadProductInfo();
+        ReadUserInfo();
+        ProcessOrder();
+
+        if ( !PromptConfirmation( PROMPT_CONTINUE_SHOPPING ) )
+        {
+            run = false;
+        }
+    }
+
+    Console.WriteLine( "Пока." );
+}
+
+void SetConsoleEncoding()
+{
+    Console.InputEncoding = System.Text.Encoding.UTF8;
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+}
+
+void ProcessOrder()
+{
+    string message = $"Здравствуйте, {userName}, вы заказали {quantity} {product} на адрес {address}, все верно? (y/n): ";
+    bool isConfirmed = PromptConfirmation( message );
+
+    if ( isConfirmed )
+    {
+        DateTime arrivalDate = DateTime.Today.AddDays( 3 );
+        PrintSuccess( userName, product, quantity, address, arrivalDate );
+    }
+    else
+    {
+        PrintCancelled();
+    }
+}
+
+void ReadProductInfo()
+{
+    product = ReadString( PROMPT_PRODUCT_TITLE );
+    quantity = ReadQuantity( PROMPT_PRODUCT_QUANTITY );
+}
+
+void ReadUserInfo()
+{
+    userName = ReadString( PROMPT_USER_NAME );
+    address = ReadString( PROMPT_USER_ADDRESS );
+}
+
+string ReadLineTrimmed()
+{
+    return Console.ReadLine()?.Trim() ?? throw new EndOfStreamException( MESSAGE_ERROR_EMPTY_STDIN );
+}
+
+string ReadString( string prompt )
+{
+    string result;
+    do
+    {
+        Console.Write( prompt );
+        result = ReadLineTrimmed();
+
+        if ( string.IsNullOrWhiteSpace( result ) )
+        {
+            Console.WriteLine( MESSAGE_ERROR_EMPTY_STRING );
+        }
+        else
+        {
+            break;
+        }
+
+    } while ( true );
+    return result;
+}
+
+int ReadQuantity( string prompt )
+{
+    int result;
+    do
+    {
+        Console.Write( prompt );
+        string input = ReadLineTrimmed();
+        if ( !int.TryParse( input, out result ) || result <= 0 )
+        {
+            Console.WriteLine( MESSAGE_ERROR_INVALID_QUANTITY );
+        }
+        else
+        {
+            break;
+        }
+
+    } while ( true );
+    return result;
+}
+
+bool PromptConfirmation( string message )
+{
+    do
+    {
+        Console.Write( message );
+        string answer = ReadLineTrimmed().ToLowerInvariant();
+
+        switch ( answer )
+        {
+            case "y":
+            case "yes":
+            case "д":
+            case "да":
+                return true;
+
+            case "n":
+            case "no":
+            case "н":
+            case "нет":
+                return false;
+
+            default:
+                Console.WriteLine( MESSAGE_ERROR_PROMPT_ANSWER );
+                break;
+        }
+    } while ( true );
+}
+
+void PrintSuccess( string name, string product, int count, string address, DateTime arrivalDate )
+{
+    string formattedDate = arrivalDate.ToString( DATE_FORMAT );
+    Console.WriteLine( $"{name}! Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} {formattedDate}" );
+}
+
+void PrintCancelled()
+{
+    Console.WriteLine( MESSAGE_ORDER_CANCELLED );
+}
